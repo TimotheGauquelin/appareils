fd489947b7d97a4f8fb2a5470e061568f422e6a0
T <gauquelintimothe@gmail.com>
2020-04-08 23:12:44 +0200

    App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'appareils';
+  isAuth = false;
+  lastUpdate = new Date();
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'Eteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'Allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'Eteint'
+    }
+  ];
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+}
 }
